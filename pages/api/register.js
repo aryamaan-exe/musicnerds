b8391fa0914a4c33e8356eb7d446ce033a4f1cd4
aryamaan-exe <aryamaan.27@gmail.com>
2025-07-23 15:19:00 +0530

    incorrect env var name

--- pages/api/register.js
+++ pages/api/register.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default async function handler(req, res) {
